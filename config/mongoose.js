7827750e3e42606f4b679b4a4825112f2932eb68
unknown <GUOCG@cn.midea.com>
2018-12-27 20:25:27 +0800

    commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,17 @@
 /**
  * Created by 1 on 2016/5/16.
  */
-var mongoose=require('mongoose');
-var config=require('./db_url.js');
+var mongoose = require('mongoose');
+var config = require('./db_url.js');
 mongoose.Promise = global.Promise;
 
-module.exports=function(){
-    var db=mongoose.connection.openUri(config.mongodb);
+module.exports = function () {
+    var db = mongoose.connection.openUri(config.mongodb);
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+        // we're connected!
+        console.log('mongodb connected');
+    });
     require('../model/user.js');
     require('../model/news.js');
     require('../model/recruit.js');
